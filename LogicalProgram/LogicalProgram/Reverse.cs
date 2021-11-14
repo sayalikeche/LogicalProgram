e7e3aa2fa4abe9ce6fe787d7f37dee60babda5fe
sayalikeche <kechesayali11@gmail.com>
2021-11-14 20:32:49 +0530

    [sayali Added Reverse]

---
+++ LogicalProgram/LogicalProgram/Reverse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class ReverseNumber
+    {
+        public void Calculation(int numb)
+        {
+            int r, reverse = 0;
+            while (numb > 0)
+            {
+                r = numb % 10;
+                reverse = reverse * 10 + r;
+                numb = numb / 10;
+            }
+        }
+    }
+}
